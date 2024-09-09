993a525f3c0c8f558e54b7340df641553a033423
Alexander-Bankov <1bankov20032mail.ru>
2024-09-09 16:24:26 +0400

    prosto

--- src/main/java/com/example/medicine/calculators/controller/FluidsMedicinePostController.java
+++ src/main/java/com/example/medicine/calculators/controller/FluidsMedicinePostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Расчет медицинских калькуляторов", description = "Возвращает результаты расчетов в формате json")
 @RequestMapping("/calculator")
 public class FluidsMedicinePostController {
+
     private final FluidsMedicineCalculator fluidsMedicineCalculator;
     @Autowired
     protected FluidsMedicinePostController(FluidsMedicineCalculator fluids){this.fluidsMedicineCalculator = fluids;}
